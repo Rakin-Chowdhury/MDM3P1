4a5c0156298272c89b42699665d4a47b8f1e32b2
RC <rc16452@bristol.ac.uk>
2019-10-21 23:22:58 +0100

    trialed simple bot movement

--- sim.py
+++ sim.py
@@ -15,21 +15,6 @@ Description:
 #Required external moduals: pygame3
 import pygame
 
-#Width and Height Dimentions
-
-
-global block_size, w, h
-block_size = 9
-w = 600
-h = 500
-win = pygame.display.set_mode((w,h))
-
-#colours
-red = ((80,25,0))
-green = ((0,255,0))
-blue = ((135,206,235))
-
-
 class Bot:
     def __init__(self, id, x, y):
         self.id = id
@@ -70,6 +55,7 @@ def drawGrid():
             #rect = pygame.Rect(x*block_size, y*block_size, block_size, block_size)
             rect = pygame.Rect(x*(block_size+10), y*(block_size+1), block_size+9, block_size)
             pygame.draw.rect(win, ((255,255,255)), rect)
+    pygame.display.update()
 
     return
 
@@ -133,56 +119,65 @@ def placePallet(id, Loc):
     return
 
 
+def main():
+
+    pygame.init()
+
+    global win, block_size, w, h
+    block_size = 9
+    w = 600
+    h = 500
+    win = pygame.display.set_mode((w,h))
+
+    #colours
+    global red, green, blue
+    red = ((80,25,0))
+    green = ((0,255,0))
+    blue = ((135,206,235))
+
+
+
+
+    pygame.font.get_fonts()
+    font = pygame.font.Font(pygame.font.get_default_font(), 24)
+    text = font.render('Key: RED = Bricks, Blue = Supply, Green = Bot', True, (0,0,0), (255,255,255))
+    textRect= text.get_rect()
+    textRect.center = (block_size*31,block_size*50 )
+
+
+
+    MainLoop = True
+
+
+    struc1 = [(x+10,10) for x in range(10)]
+    struc2 = [(10,10+x) for x in range(10)]
+
+
+    clock = pygame.time.Clock()
+    x=0
+    while MainLoop:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                MainLoop = False
+        win.fill((255,255,255))
+        drawGrid()
+
+        bot1 = placeBot(0,(8+x,8))
+        x = x+1
+
+        for i in struc1:
+            placeBrick(0,(i[0],i[1]))
+
+
+        placePallet(0,(10,7))
+
+
+        win.blit(text,textRect)
+
+        #pygame.time.delay(10)
+        clock.tick(120)
+        pygame.display.flip()
 
     return
 
-
-
-
-"""
-bot1 = Bot(1,0,0)
-rect = pygame.Rect(bot1.x*(block_size+1), bot1.y*(block_size+1), block_size, block_size)
-pygame.draw.rect(win, ((255,0,0)), rect)
-bot1.Right()
-rect = pygame.Rect(bot1.x*(block_size+1), bot1.y*(block_size+1), block_size, block_size)
-pygame.draw.rect(win, ((255,0,0)), rect)
-
-
-bot = [(1,1),(1,2),(2,1),(2,2)]
-
-for x, y in bot:
-
-
-    rect = pygame.Rect(x*(block_size+1), y*(block_size+1), block_size, block_size)
-    pygame.draw.rect(win, ((255,0,0)), rect)
-"""
-pygame.init()
-pygame.font.get_fonts()
-font = pygame.font.Font(pygame.font.get_default_font(), 24)
-text = font.render('Key: RED = Bricks, Blue = Supply, Green = Bot', True, (0,0,0), (255,255,255))
-textRect= text.get_rect()
-textRect.center = (block_size*31,block_size*50 )
-
-
-
-MainLoop = True
-
-drawGrid()
-struc1 = [(x+10,10) for x in range(10)]
-struc2 = [(10,10+x) for x in range(10)]
-
-for i in struc1:
-    placeBrick(0,(i[0],i[1]))
-
-
-placePallet(0,(10,7))
-placeBot(0,(8,8))
-
-win.blit(text,textRect)
-
-
-while MainLoop:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            MainLoop = False
-    pygame.display.flip()
+main()
